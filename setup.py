c8babed1e9d02f382511ac59dac9c115c90c1a23
shasanka296 <125386153+shasanka296@users.noreply.github.com>
2023-10-24 21:02:59 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from my_pip_package import __version__
+
+setup(
+    name='least',
+    version=1,
+
+    url='https://github.com/shasanka296/least.git',
+    author='Shasanka Lamichhane',
+    author_email='sla296@uky.edu',
+
+    py_modules=['least'],
+)
